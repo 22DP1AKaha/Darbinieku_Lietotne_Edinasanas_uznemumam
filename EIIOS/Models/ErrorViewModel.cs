ceac60a96d114133340663541a5d51a4c96e31e5
22DP1AKaha <ak00285@rvt.lv>
2025-09-02 19:17:50 +0300

    Add project files.

---
+++ EIIOS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EIIOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
