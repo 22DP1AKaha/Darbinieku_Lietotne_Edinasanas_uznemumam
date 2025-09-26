b8e3d9b81e57dbc97abc2e2836b1d1037c256a82
Rinalds <rl00392@rvt.lv>
2025-09-26 13:41:10 +0300

    Pievienoju dashboard prototipu

---
+++ EIIOS/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using EIIOS.Data;
+
+namespace EIIOS.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly EIIOSDbContext _context;
+        private readonly IStringLocalizer<DashboardController> _localizer;
+
+        public DashboardController(EIIOSDbContext context, IStringLocalizer<DashboardController> localizer)
+        {
+            _context = context;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            // Example localized values you might want to pass to the view
+            ViewBag.DashboardTitle = _localizer["Dashboard"];
+            ViewBag.ChooseAction = _localizer["ChooseAction"];
+            return View("Dashboard");
+        }
+    }
+}
