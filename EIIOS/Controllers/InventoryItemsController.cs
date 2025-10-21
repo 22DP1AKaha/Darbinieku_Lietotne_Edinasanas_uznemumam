8b1448f70acd69d052a7163e8558a4c016118481
marts <mm00448@rvt.lv>
2025-10-21 10:21:37 +0300

    bug fix

--- EIIOS/Controllers/InventoryItemsController.cs
+++ EIIOS/Controllers/InventoryItemsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 using EIIOS.Data;
 using EIIOS.Models;
 using EIIOS.Services;
@@ -11,11 +12,13 @@ namespace EIIOS.Controllers
     {
         private readonly EIIOSDbContext _context;
         private readonly UserService _userService;
+        private readonly IStringLocalizer<InventoryItemsController> _localizer;
 
-        public InventoryItemsController(EIIOSDbContext context, UserService userService)
+        public InventoryItemsController(EIIOSDbContext context, UserService userService, IStringLocalizer<InventoryItemsController> localizer)
         {
             _context = context;
             _userService = userService;
+            _localizer = localizer;
         }
 
         // GET: /InventoryItems/Manage
@@ -25,14 +28,22 @@ namespace EIIOS.Controllers
             {
                 InventoryItems = await _context.InventoryItems.OrderBy(i => i.Name).ToListAsync()
             };
+
             return View(vm);
         }
 
-        // POST: /InventoryItems/Manage
+        // POST: /InventoryItems/Manage (Add new ingredient)
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Manage(InventoryManagementViewModel model)
         {
+            if (model.NewItem.CurrentQuantity < 0)
+                ModelState.AddModelError("NewItem.CurrentQuantity", _localizer["QuantityCannotBeNegative"]);
+            if (model.NewItem.MinimumQuantity < 0)
+                ModelState.AddModelError("NewItem.MinimumQuantity", _localizer["MinimumQuantityCannotBeNegative"]);
+            if (model.NewItem.UnitCost < 0)
+                ModelState.AddModelError("NewItem.UnitCost", _localizer["UnitCostCannotBeNegative"]);
+
             if (!ModelState.IsValid)
             {
                 model.InventoryItems = await _context.InventoryItems.OrderBy(i => i.Name).ToListAsync();
@@ -51,7 +62,7 @@ namespace EIIOS.Controllers
             _context.InventoryItems.Add(newItem);
             await _context.SaveChangesAsync();
 
-            TempData["Success"] = "Ingredient added successfully.";
+            TempData["Success"] = _localizer["IngredientAdded"].Value;
             return RedirectToAction(nameof(Manage));
         }
 
@@ -59,7 +70,8 @@ namespace EIIOS.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             var item = await _context.InventoryItems.FindAsync(id);
-            if (item == null) return NotFound();
+            if (item == null)
+                return NotFound();
 
             return View(item);
         }
@@ -70,18 +82,28 @@ namespace EIIOS.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var item = await _context.InventoryItems.FindAsync(id);
-            if (item == null) return NotFound();
+            if (item == null)
+                return NotFound();
 
             _context.InventoryItems.Remove(item);
             await _context.SaveChangesAsync();
 
-            TempData["Success"] = "Ingredient removed successfully.";
+            TempData["Success"] = _localizer["IngredientRemoved"].Value;
             return RedirectToAction(nameof(Manage));
         }
-        // POST: /InventoryItems/Update/5
+
+        // POST: /InventoryItems/Edit
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(InventoryItemModel model)
         {
+            if (model.CurrentQuantity < 0)
+                ModelState.AddModelError(nameof(model.CurrentQuantity), _localizer["QuantityCannotBeNegative"]);
+            if (model.MinimumQuantity < 0)
+                ModelState.AddModelError(nameof(model.MinimumQuantity), _localizer["MinimumQuantityCannotBeNegative"]);
+            if (model.UnitCost < 0)
+                ModelState.AddModelError(nameof(model.UnitCost), _localizer["UnitCostCannotBeNegative"]);
+
             if (!ModelState.IsValid)
                 return RedirectToAction(nameof(Manage));
 
@@ -93,37 +115,14 @@ namespace EIIOS.Controllers
             item.Unit = model.Unit;
             item.CurrentQuantity = model.CurrentQuantity;
             item.MinimumQuantity = model.MinimumQuantity;
+            item.UnitCost = model.UnitCost;
             item.UpdatedAt = DateTime.UtcNow;
 
             _context.InventoryItems.Update(item);
             await _context.SaveChangesAsync();
 
+            TempData["Success"] = _localizer["IngredientUpdated"].Value;
             return RedirectToAction(nameof(Manage));
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Add(InventoryManagementViewModel vm)
-        {
-            if (!ModelState.IsValid)
-            {
-                vm.InventoryItems = await _context.InventoryItems.OrderBy(i => i.Name).ToListAsync();
-                return View("Manage", vm);
-            }
-
-            var newItem = new InventoryItemModel
-            {
-                Name = vm.NewItem.Name,
-                Unit = vm.NewItem.Unit,
-                CurrentQuantity = vm.NewItem.CurrentQuantity,
-                MinimumQuantity = vm.NewItem.MinimumQuantity
-            };
-
-            _context.InventoryItems.Add(newItem);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(Manage));
-        }
-
     }
 }
